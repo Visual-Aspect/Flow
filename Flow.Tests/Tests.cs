e385f97ea758b1acb1f726db525915fafab4546c
NebulaDev <nebuladev.contact@gmail.com>
2023-05-15 17:24:57 +1200

    Add base class

---
+++ Flow.Tests/Tests.cs
@@ -0,0 +1,8 @@
+namespace FlowNS.Tests;
+
+public class UnitTest1 {
+    [Fact]
+    public void Test() {
+        Flow.Temp();
+    }
+}
---
+++ Flow/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System;
+using System.Text.RegularExpressions;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FlowNS;
+
+public static partial class Flow {
+    public static void Temp() {
+        Console.WriteLine("Hello, world!");
+    }
+}
